69518531fd2c0024dabcb6695106e6cb4d0590b5
nurofsun <dev.nurahmat@gmail.com>
2021-01-01 10:37:43 +0700

    chore: basic next method

---
+++ dist/js/onescroll.js
@@ -0,0 +1,43 @@
+class OneScroll {
+  constructor(selector) {
+    this.selector = selector;
+  }
+
+  getHeightSection() {
+    let sections = document.querySelectorAll(`${this.selector} > .onescroll-container > .onescroll-section`);
+    return sections[0].clientHeight;
+  }
+
+  getContainer() {
+    return document.querySelector(`${this.selector} > .onescroll-container`);
+  }
+
+  getContainerCurrentPosition() {
+    let container = this.getContainer();
+    return parseInt(container.style.bottom);
+  }
+
+  init() {
+    let container = this.getContainer();
+    container.style.bottom = 0;
+  }
+
+  next() {
+    let container = this.getContainer();
+    let currentPosition = this.getContainerCurrentPosition();
+    let heightOfEachSection = this.getHeightSection();
+    container.style.bottom = currentPosition + heightOfEachSection + 'px';
+    console.log(currentPosition);
+  }
+
+  previous() {
+    let {
+      container,
+      heightOfEachSection,
+      currentPosition
+    } = this.init();
+    currentPosition -= heightOfEachSection;
+    container.style.bottom = currentPosition + 'px';
+  }
+
+}
