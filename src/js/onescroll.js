d5d233a8d16822fd09f95974564f5f22bdc4c62d
nurofsun <dev.nurahmat@gmail.com>
2021-01-01 18:17:59 +0700

    chore: added move section onwheel event
    description: This feature not already perfect yet, maybe there's a bug
    in the future

--- src/js/onescroll.js
+++ src/js/onescroll.js
@@ -7,27 +7,55 @@ class OneScroll {
 		return sections[0].clientHeight;
 	}
 	getContainer() {
-		return document
-				.querySelector(`${this.selector} > .onescroll-container`);
+		return document.querySelector(`${this.selector} > .onescroll-container`);
 	}
 	getContainerCurrentPosition() {
-		let container = this.getContainer();
-		return parseInt(container.style.bottom);
+		return parseInt(this.getContainer().style.bottom);
 	}
-	init() {
+	setInitialContainerPosition() {
 		let container = this.getContainer();
 		container.style.bottom = 0;
 	}
-	next() {
+	moveDown() {
 		let container = this.getContainer();
 		let currentPosition = this.getContainerCurrentPosition();
 		let heightOfEachSection = this.getHeightSection();
 		container.style.bottom = (currentPosition + heightOfEachSection) + 'px';
-		console.log(currentPosition);
 	}
-	previous() {
-		let { container, heightOfEachSection, currentPosition } = this.init();
-		currentPosition -= heightOfEachSection;
-		container.style.bottom = currentPosition + 'px';
+	moveUp() {
+		let container = this.getContainer();
+		let currentPosition = this.getContainerCurrentPosition();
+		let heightOfEachSection = this.getHeightSection();
+		container.style.bottom = (currentPosition - heightOfEachSection) + 'px';
+	}
+	init() {
+		this.setInitialContainerPosition();
+
+		let sections = document.querySelectorAll('.onescroll-section');
+
+		const moveSection = (event) => {
+			if (event.deltaY < 0) {
+				if (this.getContainerCurrentPosition() !== 0) {
+					this.moveUp();	
+					document.body.removeEventListener('wheel', moveSection);
+				}
+			}
+			else if (event.deltaY > 0) {
+				if (this.getContainerCurrentPosition() < (this.getHeightSection() * (sections.length - 1))) {
+					this.moveDown();	
+					document.body.removeEventListener('wheel', moveSection);
+				}
+			}
+			else {
+				return false;
+			}
+			setTimeout(function() {
+				document.body.addEventListener('wheel', moveSection);
+			}, 800)
+		};
+		//sections.forEach(function(section) {
+			//section.addEventListener('wheel', moveSection);
+		//});
+		document.body.addEventListener('wheel', moveSection);
 	}
 }
