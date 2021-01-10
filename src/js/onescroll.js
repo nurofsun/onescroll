38cd4df87f882577aa11df16060d98b26dfecee6
nurofsun <dev.nurahmat@gmail.com>
2021-01-10 11:10:25 +0700

    chore: added support onescroll for touch devices

--- src/js/onescroll.js
+++ src/js/onescroll.js
@@ -2,6 +2,9 @@ class OneScroll {
 	constructor(selector) {
 		this.selector = selector;
 	}
+	getScreenSize() {
+		return window.matchMedia('(max-width: 768px)');
+	}
 	getHeightSection() {
 		let sections = document.querySelectorAll(`${this.selector} > .onescroll-container > .onescroll-section`);
 		return sections[0].clientHeight;
@@ -30,32 +33,57 @@ class OneScroll {
 	}
 	init() {
 		this.setInitialContainerPosition();
-
 		let sections = document.querySelectorAll('.onescroll-section');
 
-		const moveSection = (event) => {
-			if (event.deltaY < 0) {
-				if (this.getContainerCurrentPosition() !== 0) {
-					this.moveUp();	
-					document.body.removeEventListener('wheel', moveSection);
+		let currentScreenSize = this.getScreenSize();
+
+		window.addEventListener('resize', function(event) {
+			currentScreenSize = this.getScreenSize();
+		});
+
+		if (!currentScreenSize.matches) {
+			const moveSection = (event) => {
+				if (event.deltaY < 0) {
+					if (this.getContainerCurrentPosition() !== 0) {
+						this.moveUp();	
+						document.body.removeEventListener('wheel', moveSection);
+					}
+				}
+				else if (event.deltaY > 0) {
+					if (this.getContainerCurrentPosition() < (this.getHeightSection() * (sections.length - 1))) {
+						this.moveDown();	
+						document.body.removeEventListener('wheel', moveSection);
+					}
+				}
+				else {
+					return false;
+				}
+				setTimeout(function() {
+					document.body.addEventListener('wheel', moveSection);
+				}, 700)
+			};
+			//sections.forEach(function(section) {
+				//section.addEventListener('wheel', moveSection);
+			//});
+			document.body.addEventListener('wheel', moveSection);
+		} else {
+			const moveSectionForMobile = (event) => {
+				let userTouch = event.changedTouches[0];
+				let sectionHeightCenter = this.getHeightSection() / 2;
+
+				if (sectionHeightCenter > userTouch.clientY) {
+					if (this.getContainerCurrentPosition() !== 0) {
+						this.moveUp();	
+					}
+				}
+				else {
+					if (this.getContainerCurrentPosition() < (this.getHeightSection() * (sections.length - 1))) {
+						this.moveDown();	
+					}
 				}
 			}
-			else if (event.deltaY > 0) {
-				if (this.getContainerCurrentPosition() < (this.getHeightSection() * (sections.length - 1))) {
-					this.moveDown();	
-					document.body.removeEventListener('wheel', moveSection);
-				}
-			}
-			else {
-				return false;
-			}
-			setTimeout(function() {
-				document.body.addEventListener('wheel', moveSection);
-			}, 700)
-		};
-		//sections.forEach(function(section) {
-			//section.addEventListener('wheel', moveSection);
-		//});
-		document.body.addEventListener('wheel', moveSection);
+
+			document.body.addEventListener('touchend', moveSectionForMobile, false);
+		}
 	}
 }
