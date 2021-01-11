a4b4a8787b3214dc89bb1fe0ca76b4689eb918ee
nurofsun <dev.nurahmat@gmail.com>
2021-01-12 06:46:39 +0700

    chore: bug fixed, add new feature
    new feature:
    move the section by index, it could be implemented on button click event
    listener as well

--- src/js/onescroll.js
+++ src/js/onescroll.js
@@ -2,9 +2,6 @@ class OneScroll {
 	constructor(selector) {
 		this.selector = selector;
 	}
-	getScreenSize() {
-		return window.matchMedia('(max-width: 768px)');
-	}
 	getHeightSection() {
 		let sections = document.querySelectorAll(`${this.selector} > .onescroll-container > .onescroll-section`);
 		return sections[0].clientHeight;
@@ -31,59 +28,64 @@ class OneScroll {
 		let heightOfEachSection = this.getHeightSection();
 		container.style.bottom = (currentPosition - heightOfEachSection) + 'px';
 	}
+	moveByIndex(number) {
+		let container = this.getContainer();
+		let heightOfEachSection = this.getHeightSection();
+		container.style.bottom = (heightOfEachSection * number) + 'px';
+	}
 	init() {
 		this.setInitialContainerPosition();
+
 		let sections = document.querySelectorAll('.onescroll-section');
 
-		let currentScreenSize = this.getScreenSize();
+		const attachAction = () => {
+			let mobileScreenSize = window.matchMedia('(max-width: 768px)');
 
-		window.addEventListener('resize', function(event) {
-			currentScreenSize = this.getScreenSize();
-		});
+			if (!mobileScreenSize.matches) {
+				const moveSection = (event) => {
+					if (event.deltaY < 0) {
+						if (this.getContainerCurrentPosition() !== 0) {
+							this.moveUp();	
+							document.body.removeEventListener('wheel', moveSection);
+						}
+					}
+					else if (event.deltaY > 0) {
+						if (this.getContainerCurrentPosition() < (this.getHeightSection() * (sections.length - 1))) {
+							this.moveDown();	
+							document.body.removeEventListener('wheel', moveSection);
+						}
+					}
+					else {
+						return false;
+					}
+					setTimeout(function() {
+						document.body.addEventListener('wheel', moveSection);
+					}, 700)
+				};
+				//sections.forEach(function(section) {
+					//section.addEventListener('wheel', moveSection);
+				//});
+				document.body.addEventListener('wheel', moveSection);
+			} else if (mobileScreenSize.matches) {
+				const moveSectionForMobile = (event) => {
+					let userTouch = event.changedTouches[0];
+					let sectionHeightCenter = this.getHeightSection() / 2;
 
-		if (!currentScreenSize.matches) {
-			const moveSection = (event) => {
-				if (event.deltaY < 0) {
-					if (this.getContainerCurrentPosition() !== 0) {
-						this.moveUp();	
-						document.body.removeEventListener('wheel', moveSection);
+					if (sectionHeightCenter > userTouch.clientY) {
+						if (this.getContainerCurrentPosition() < (this.getHeightSection() * (sections.length - 1))) {
+							this.moveDown();	
+						}
+					}
+					else {
+						if (this.getContainerCurrentPosition() !== 0) {
+							this.moveUp();	
+						}
 					}
 				}
-				else if (event.deltaY > 0) {
-					if (this.getContainerCurrentPosition() < (this.getHeightSection() * (sections.length - 1))) {
-						this.moveDown();	
-						document.body.removeEventListener('wheel', moveSection);
-					}
-				}
-				else {
-					return false;
-				}
-				setTimeout(function() {
-					document.body.addEventListener('wheel', moveSection);
-				}, 700)
-			};
-			//sections.forEach(function(section) {
-				//section.addEventListener('wheel', moveSection);
-			//});
-			document.body.addEventListener('wheel', moveSection);
-		} else {
-			const moveSectionForMobile = (event) => {
-				let userTouch = event.changedTouches[0];
-				let sectionHeightCenter = this.getHeightSection() / 2;
-
-				if (sectionHeightCenter > userTouch.clientY) {
-					if (this.getContainerCurrentPosition() < (this.getHeightSection() * (sections.length - 1))) {
-						this.moveDown();	
-					}
-				}
-				else {
-					if (this.getContainerCurrentPosition() !== 0) {
-						this.moveUp();	
-					}
-				}
+				document.body.addEventListener('touchend', moveSectionForMobile, false);
 			}
-
-			document.body.addEventListener('touchend', moveSectionForMobile, false);
 		}
+		window.addEventListener('resize', attachAction);
+		attachAction();
 	}
 }
